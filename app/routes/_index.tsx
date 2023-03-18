ebd000cf7a396af476253ef3978396e94ff9050d
tokerranta <rantanikunen.markus@gmail.com>
2023-03-18 09:27:04 +0100

    modified home page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,6 +3,9 @@ import PageTitle from "~/components/PageTitle";
 
 export default function Index() {
   return (
-    <Main><PageTitle>Welcome Home</PageTitle></Main>
+    <Main>
+      <PageTitle>Welcome Home</PageTitle>
+      <h1 className="text-4xl text-center">I created this site to play around with <a className="text-orange-400 underline" href="https://remix.run/" target="_blank" rel="noreferrer">Remix</a></h1>
+    </Main>
   );
 }
